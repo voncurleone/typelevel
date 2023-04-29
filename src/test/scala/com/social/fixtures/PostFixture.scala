ff3c4e4ba15dcf69b9fbe042a2cdd369e49420db
Tim <voncurleone@gmail.com>
2023-04-29 13:25:53 -0400

    Testing posts http api.

---
+++ src/test/scala/com/social/fixtures/PostFixture.scala
@@ -0,0 +1,75 @@
+package com.social.fixtures
+
+import cats.syntax.all.*
+import com.social.domain.Post.{Post, PostInfo}
+
+import java.util.UUID
+
+trait PostFixture {
+
+  val NotFoundPostUuid: UUID = UUID.fromString("6ea79557-3112-4c84-a8f5-1d1e2c300948")
+
+  val AwesomePostUuid: UUID = UUID.fromString("843df718-ec6e-4d49-9289-f799c0f40064")
+
+  val AwesomePost: Post = Post(
+    AwesomePostUuid,
+    659186086L,
+    "voncurleone@gmail.com",
+    PostInfo(
+      "Awesome post!",
+      7,
+      2,
+      Some(List("tag1", "tag2")),
+      Some("image?!?")
+    ),
+    false
+  )
+
+  val InvalidPost: Post = Post(
+    null,
+    42L,
+    "email@gmail.com",
+    PostInfo.empty,
+    false
+  )
+
+  val UpdatedAwesomePost: Post = Post(
+    AwesomePostUuid,
+    659186086L,
+    "voncurleone@gmail.com",
+    PostInfo(
+      "Updated",
+      10,
+      32,
+      Some(List("tag1", "tag3")),
+      None
+    ),
+    false
+  )
+
+  val NewPostInfo: PostInfo = PostInfo(
+    "A new post.",
+    35,
+    3,
+    None,
+    Some("image!!!!!!")
+  )
+
+
+  val AwesomePostWithIdNotFound: Post = AwesomePost.copy(id = NotFoundPostUuid)//AwesomeJob.copy(id = NotFoundJobUuid)
+
+  val AnotherAwesomePostUuid: UUID = UUID.fromString("19a941d0-aa19-477b-9ab0-a7033ae65c2b")
+  val AnotherAwesomePost: Post = AwesomePost.copy(id = AnotherAwesomePostUuid)
+
+  val TestAwesomePost: Post =
+    AwesomePost.copy(postInfo = AwesomePost.postInfo.copy(text = "Test"))
+
+  val NewPostUuid: UUID = UUID.fromString("efcd2a64-4463-453a-ada8-b1bae1db4377")
+  val AwesomeNewPostInfo: PostInfo = PostInfo(
+    "New awesome post!",
+    67,
+    87,
+    None,
+    None
+  )
+}
---
+++ src/test/scala/com/social/http/routes/JobRoutesSpec.scala
@@ -0,0 +1,136 @@
+package com.social.http.routes
+
+import cats.effect.*
+import cats.implicits.*
+import cats.effect.testing.scalatest.AsyncIOSpec
+import com.social.core.Posts
+import com.social.domain.Post.{Post, PostInfo}
+import com.social.fixtures.PostFixture
+import org.http4s.{HttpRoutes, Method, Request, Status}
+import org.http4s.dsl.Http4sDsl
+import org.http4s.implicits.uri
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import io.circe.generic.auto.*
+import org.http4s.circe.CirceEntityCodec.*
+import org.scalatest.matchers.should.Matchers.shouldBe
+
+import java.util.UUID
+
+class JobRoutesSpec
+  extends AsyncFreeSpec
+  with AsyncIOSpec
+  with Matchers
+  with Http4sDsl[IO]
+  with PostFixture {
+
+  //Prep
+  //Mock the Posts module
+  val posts: Posts[IO] = new Posts[IO] {
+    def create(ownerEmail: String, postInfo: PostInfo): IO[UUID] = IO.pure(NewPostUuid)
+
+    def all(): IO[List[Post]] = IO.pure(List(AwesomePost))
+
+    def find(id: UUID): IO[Option[Post]] =
+      if id == AwesomePostUuid then IO.pure(Some(AwesomePost))
+      else IO.pure(None)
+
+    def update(id: UUID, postInfo: PostInfo): IO[Option[Post]] =
+      if id == AwesomePostUuid then IO.pure(Some(UpdatedAwesomePost))
+      else IO.pure(None)
+
+    def hide(id: UUID): IO[Int] =
+      if id == AwesomePostUuid then IO.pure(1)
+      else IO.pure(0)
+
+    def delete(id: UUID): IO[Int] =
+      if id == AwesomePostUuid then IO.pure(1)
+      else IO.pure(0)
+  }
+
+  //build jobsRoutes
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  val postRoutes: HttpRoutes[IO] = PostRoutes[IO](posts).routes
+
+  //Tests
+  "PostRoutes" - {
+    "should return a post with a given id" in {
+      for {
+        //simulate an http request
+        response <- postRoutes.orNotFound.run {
+          Request(method = Method.GET, uri = uri"/posts/843df718-ec6e-4d49-9289-f799c0f40064")
+        }
+
+        retrieved <- response.as[Post]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe AwesomePost
+      }
+    }
+
+    "should return all posts" in {
+      for {
+        //simulate an http request
+        response <- postRoutes.orNotFound.run {
+          Request(method = Method.POST, uri = uri"/posts")
+        }
+
+        retrieved <- response.as[List[Post]]
+      } yield {
+        response.status shouldBe Status.Ok
+        retrieved shouldBe List(AwesomePost)
+      }
+    }
+
+    "should create a new post" in {
+      for {
+        //simulate an http request
+        response <- postRoutes.orNotFound.run {
+          Request(method = Method.POST, uri = uri"/posts/create")
+            .withEntity(AwesomePost.postInfo)
+        }
+
+        retrieved <- response.as[UUID]
+      } yield {
+        response.status shouldBe Status.Created
+        retrieved shouldBe NewPostUuid
+      }
+    }
+
+    "should only update a post that exists" in {
+      for {
+        //simulate an http request
+        responseOk <- postRoutes.orNotFound.run {
+          Request(method = Method.PUT, uri = uri"/posts/843df718-ec6e-4d49-9289-f799c0f40064")
+            .withEntity(UpdatedAwesomePost.postInfo)
+        }
+
+        response404 <- postRoutes.orNotFound.run {
+          Request(method = Method.PUT, uri = uri"/posts/843df718-ec6e-4d49-9289-f799c0f40000")
+            .withEntity(UpdatedAwesomePost.postInfo)
+        }
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        response404.status shouldBe Status.NotFound
+      }
+    }
+
+    "should only delete a post that exists" in {
+      for {
+        //simulate an http request
+        responseOk <- postRoutes.orNotFound.run {
+          Request(method = Method.DELETE, uri = uri"/posts/843df718-ec6e-4d49-9289-f799c0f40064")
+        }
+
+        response404 <- postRoutes.orNotFound.run {
+          Request(method = Method.DELETE, uri = uri"/posts/843df718-ec6e-4d49-9289-f799c0f40000")
+        }
+      } yield {
+        responseOk.status shouldBe Status.Ok
+        response404.status shouldBe Status.NotFound
+      }
+    }
+  }
+}
