80126310a55b90b3519dd38f917da06427876fa2
Tim <voncurleone@gmail.com>
2023-05-09 19:25:09 -0400

    Added users table to database init script.

--- sql/init.sql
+++ sql/init.sql
@@ -15,3 +15,15 @@ CREATE TABLE posts(
 
 ALTER TABLE posts
 ADD CONSTRAINT pk_posts PRIMARY KEY (id);
+
+CREATE TABLE users(
+                      email text NOT NULL,
+                      handle text NOT NULL,
+                      hashedPass text NOT NULL,
+                      firstName text,
+                      lastName text,
+                      role text NOT NULL
+);
+
+ALTER TABLE users
+ADD CONSTRAINT pk_users PRIMARY KEY (email);
