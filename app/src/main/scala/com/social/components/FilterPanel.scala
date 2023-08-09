b2477a701d24b0c08c48693d5eeaedcc41d09f4e
Tim <voncurleone@gmail.com>
2023-08-09 09:50:27 -0400

    Completed filterPanel ui. The tag filters are currently broken. Need to fix that at some point.

--- app/src/main/scala/com/social/components/FilterPanel.scala
+++ app/src/main/scala/com/social/components/FilterPanel.scala
@@ -10,10 +10,17 @@ import tyrian.Html.*
 import tyrian.http.{HttpError, Method, Response}
 import io.circe.generic.auto.*
 import FilterPanel.*
+import org.scalajs.dom.HTMLInputElement
+import tyrian.cmds.Logger
 
 case class FilterPanel (
     filters: PostFilter = PostFilter(),
-    error: Option[String] = None
+    error: Option[String] = None,
+    minLikes: Int = 0,
+    minDisLikes: Int = 0,
+    selectedFilters: Map[String, Set[String]] = Map(),
+    dirty: Boolean = false,
+    filterAction: Map[String, Set[String]] => App.Msg = _ => App.NoOp
                        ) extends Component[App.Msg, FilterPanel] {
   override def initCmd: Cmd[IO, App.Msg] =
     Commands.getFilters
@@ -25,24 +32,113 @@ case class FilterPanel (
     case FilterPanelError(error) =>
       (this.copy(error = Some(error)), Cmd.None)
 
+    case UpdateLikesInput(likes) =>
+      (this.copy(minLikes = likes, dirty = true), Cmd.None)
+
+    case UpdateDisLikesInput(dislikes) =>
+      (this.copy(minDisLikes = dislikes, dirty = true), Cmd.None)
+
+    case UpdateCheckBoxs(groupName, value, checked) =>
+      val oldGroup = selectedFilters.getOrElse(groupName, Set())
+      val newGroup = if(checked) oldGroup + value else oldGroup - value
+      val newGroups = selectedFilters + (groupName -> newGroup)
+      (this.copy(selectedFilters = newGroups, dirty = true), Logger.consoleLog[IO](s"Filters: $newGroups"))
+
+    case TriggerFilter =>
+      (this.copy(dirty = false), Cmd.emit(filterAction(selectedFilters)))
+
     case _ => (this, Cmd.None)
 
   override def view(): Html[App.Msg] =
     div(`class` := "filter-panel-container")(
       renderError(),
-      div(filters.toString)
+      renderLikesFilter(),
+      renderDisLikesFilter(),
+      renderCheckBoxGroup("tags", filters.tags),
+      renderFiltersButton()
     )
 
+  //ui
   private def renderError() =
     error.map { e =>
       div(`class` := "filter-panel-error")(e)
     }.getOrElse(div())
+
+  private def renderLikesFilter() =
+    div(`class` := "filter-group")(
+      h6(`class` := "filter-group-header")("likes"),
+      div(`class` := "filter-group-content")(
+        label(`for` := "filter-likes")("minimum likes"),
+        input(
+          `type` := "number",
+          id := "filter-likes",
+          onInput(likes => if(likes.isEmpty) UpdateLikesInput(0) else UpdateLikesInput(likes.toInt))
+        )
+      )
+    )
+
+  private def renderDisLikesFilter() =
+    div(`class` := "filter-group")(
+      h6(`class` := "filter-group-header")("dislikes"),
+      div(`class` := "filter-group-content")(
+        label(`for` := "filter-dislikes")("minimum dislikes"),
+        input(
+          `type` := "number",
+          id := "filter-dislikes",
+          onInput(likes => if (likes.isEmpty) UpdateDisLikesInput(0) else UpdateDisLikesInput(likes.toInt))
+        )
+      )
+    )
+
+  private def renderCheckBoxGroup(groupName: String, values: List[String]) =
+    val selectedValues = selectedFilters.getOrElse(groupName, Set())
+
+    div(`class` := "filter-group")(
+      h6(`class` := "filter-group-header")(groupName),
+      div(`class` := "filter-group-content")(
+        values.map { value =>
+          renderCheckBox(groupName, value, selectedValues)
+        }
+      )
+    )
+
+  private def renderCheckBox(groupName: String, value: String, selectedValues: Set[String]) =
+    div(`class` := "filter-group-content")(
+      label(`for` := s"filter-$groupName-$value")(value),
+      input(
+        `type` := "checkbox",
+        id := s"filter-$groupName-$value",
+        checked(selectedValues.contains(value)),
+        onEvent(
+          "change",
+          event => {
+            val checkbox = event.target.asInstanceOf[HTMLInputElement]
+            UpdateCheckBoxs(groupName, value, checkbox.checked)
+          }
+        )
+      )
+    )
+
+  private def renderFiltersButton() =
+    button(
+      `type` := "button",
+      disabled(!dirty),
+      onClick(TriggerFilter)
+    )("Apply filyers")
 }
 
 object FilterPanel {
   trait Msg extends App.Msg
   case class FilterPanelError(error: String) extends Msg
   case class SetFilters(filters: PostFilter) extends Msg
+
+  //content
+  case class UpdateLikesInput(likes: Int) extends Msg
+  case class UpdateDisLikesInput(dislikes: Int) extends Msg
+  case class UpdateCheckBoxs(groupName: String, value: String, checked: Boolean) extends Msg
+
+  //action
+  case object TriggerFilter extends Msg
 
   object EndPoints {
     val getFilters: Endpoint[Msg] = new Endpoint[Msg] {
--- app/src/main/scala/com/social/pages/PostFeedPage.scala
+++ app/src/main/scala/com/social/pages/PostFeedPage.scala
@@ -10,19 +10,23 @@ import com.social.components.FilterPanel
 import tyrian.http.{HttpError, Method, Response, Status}
 import io.circe.parser.*
 import io.circe.generic.auto.*
+import tyrian.cmds.Logger
 
 final case class PostFeedPage(
-    filterPanel: FilterPanel = FilterPanel(),
+    filterPanel: FilterPanel = FilterPanel(
+      filterAction = FilterPosts.apply
+    ),
     posts: List[Post] = List(),
     canLoadMore: Boolean = true,
-    status: Option[Page.Status] = Some(Page.Status("Loading", Page.StatusKind.LOADING))
+    status: Option[Page.Status] = Some(Page.Status("Loading", Page.StatusKind.LOADING)),
+    postFilter: PostFilter = PostFilter()
                              ) extends Page {
   override def initCmd: Cmd[IO, Page.Msg] =
     Commands.getPosts() |+| filterPanel.initCmd
 
   override def update(msg: Page.Msg): (Page, Cmd[IO, Page.Msg]) = msg match
     case AddPosts(list, clm) =>
-      (setSuccessStatus("Loaded").copy(posts = this.posts ++ list, canLoadMore = clm), Cmd.None)
+      (setSuccessStatus("Loaded").copy(posts = this.posts ++ list, canLoadMore = clm), Logger.consoleLog[IO]("added"))
 
     case SetErrorStatus(e) =>
       (setSuccessStatus(e), Cmd.None)
@@ -33,6 +37,10 @@ final case class PostFeedPage(
     case msg: FilterPanel.Msg =>
       val (newFilterPanel, cmd) = filterPanel.update(msg)
       (this.copy(filterPanel = newFilterPanel), cmd)
+
+    case FilterPosts(selectedFilters) =>
+      val newPostFilter = createPostFilter(selectedFilters)
+      (this.copy(posts = List(),postFilter = newPostFilter), Commands.getPosts(newPostFilter))
 
     case _ =>
       (this, Cmd.None)
@@ -66,7 +74,6 @@ final case class PostFeedPage(
       //div(s"${post.email} - ${post.postInfo.text}")
     )
 
-  //ui
   private def maybeRenderLoadMore: Option[Html[App.Msg]] = status.map { s =>
     div(`class` := "load-more-action")(
       s match
@@ -86,6 +93,13 @@ final case class PostFeedPage(
 
   def setSuccessStatus(message: String): PostFeedPage =
     this.copy(status = Some(Page.Status(message = message, kind = Page.StatusKind.SUCCESS)))
+
+  private def createPostFilter(selectedFilters: Map[String, Set[String]]) =
+    PostFilter(
+      likes = Some(filterPanel.minLikes),
+      dislikes = Some(filterPanel.minDisLikes),
+      tags = selectedFilters.getOrElse("tags", Set()).toList
+    )
 }
 
 object PostFeedPage {
@@ -95,6 +109,7 @@ object PostFeedPage {
 
   //action
   case object LoadMorePosts extends Msg
+  case class FilterPosts(selectedFilters: Map[String, Set[String]]) extends Msg
 
   object Endpoints {
     def getPosts(limit: Int = Constants.defaultPageSize, offset: Int = 0): Endpoint[Msg] = new Endpoint[Msg] {
